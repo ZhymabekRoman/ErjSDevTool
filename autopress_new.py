b9437095dc25bb851d42b02f4c8aaefa37c4c0c7
ZhymabekRoman <robanokssamit@yandex.ru>
2023-10-09 12:18:42 +0300

    UPDATE

--- autopress_new.py
+++ autopress_new.py
@@ -2,12 +2,15 @@ import pyautogui
 from aiohttp import web
 import asyncio
 
+pyautogui.FAILSAFE = False
+
+
 async def f9_press_func():
-    await asyncio.sleep(3)
+    await asyncio.sleep(5)
     while True:
-        await asyncio.sleep(0.4)
-        print("Press f9...")
-        # pyautogui.press('f9')
+        await asyncio.sleep(0.01)
+        # print("Press f9...")
+        pyautogui.press('f9')
 
 async def hello(request):
     exit(1)
